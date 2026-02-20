68f59400868ee977df6986323e0dd71719aa7a15
Andrea Bergia <andreabergia@gmail.com>
2026-02-20 23:45:18 +0100

    test: replace deprecated assert_cmd cargo_bin helper

--- tests/cli_smoke.rs
+++ tests/cli_smoke.rs
@@ -1,8 +1,8 @@
-use assert_cmd::Command;
+use assert_cmd::cargo::cargo_bin_cmd;
 use predicates::str::contains;
 
 #[test]
 fn help_command_works() {
-    let mut cmd = Command::cargo_bin("tqs").expect("binary should build");
+    let mut cmd = cargo_bin_cmd!("tqs");
     cmd.arg("--help").assert().success().stdout(contains("tqs"));
 }
